7c27f230daf504e87d143ef8de3f94bcef77e595
AlekseyVoronkov <voronkovlesha42@mail.ru>
2025-05-05 19:49:13 +0700

    Reapply "Some shop improvements +unfinished dash ability."
    This reverts commit a96d684fa9adc14cdd885e752a9b08cb179e681b.

---
+++ Practice i guess/ShopPool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_i_guess
+{
+    internal static class ShopPool
+    {
+        internal struct portalUpgrade
+        {
+            public static bool isBought = false;
+        }
+
+        internal struct dashUpgrade
+        {
+            public static bool isBought = false;
+        }
+    }
+}
